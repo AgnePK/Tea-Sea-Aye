ac786b5341e51ba5d18d89a3e0a79f924acb24b5
Agne Postnikova <N00211108@iadt.ie>
2022-10-28 18:57:33 +0100

    Designed view all page and single view

--- resources/views/teas/index.blade.php
+++ resources/views/teas/index.blade.php
@@ -12,14 +12,28 @@
             </x-alert-success> --}}
             <a href="{{ route('teas.create') }}" class="btn-link btn-lg mb-2">+ New tea</a>
             @forelse ($teas as $tea)
-                <div class="my-6 p-6 bg-white border-b border-gray-200 shadow-sm sm:rounded-lg">
-                    <h2 class="font-bold text-2xl">
-                        <a href="{{ route('teas.show', $tea->id) }}">{{ $tea->name }}</a>
-                    </h2>
-                    <p class="mt-2">
-                        {{ Str::limit($tea->description, 200) }}
-                    </p>
-                    <span class="block mt-4 text-sm opacity-70">{{ $tea->updated_at->diffForHumans() }}</span>
+                <div class="my-6 p-6 bg-white border-b border-gray-200 shadow-sm sm:rounded-lg flex">
+                    <div class="mr-6">
+                        <img src="{{ asset('storage/images/lyons_tea.jpeg') }}" width="160">
+                    </div>
+                    <div>
+                        <div class="flex">
+                            <h2 class="font-bold text-2xl">
+                                <a href="{{ route('teas.show', $tea->id) }}">{{ $tea->name }}</a>
+                            </h2> 
+                            <p class="ml-2 text-2xl">
+                                ·    {{$tea->brand }}
+                            </p>
+                        </div>
+                        <p class="mt-2">
+                            {{ Str::limit($tea->description, 140) }}
+                        </p>
+                        <p class="mt-4 font-bold font-serif text-xl">
+                            €{{$tea->price }}
+                        </p>
+                        <span class="block mt-4 text-sm opacity-70">{{ $tea->updated_at->diffForHumans() }}</span>
+                    </div>
+
                 </div>
             @empty
             <p>You have no teas yet.</p>
--- resources/views/teas/show.blade.php
+++ resources/views/teas/show.blade.php
@@ -20,13 +20,26 @@
                     @csrf
                     <button type="submit" class="btn btn-danger ml-4" onclick="return confirm('Are you sure you wish to delete this tea?')">Delete tea</button>
             </div>
-            <div class="my-6 p-6 bg-white border-b border-gray-200 shadow-sm sm:rounded-lg">
-                <img src="{{ asset('storage/images/lyons_tea.jpeg') }}" width="150">
-                <h2 class="font-bold text-4xl">
-                    {{ $tea->name }}
-                </h2>
-                <p class="mt-6 whitespace-">{{$tea->description}}</p>
+
+
+            <div class="my-6 p-6 bg-white border-b border-gray-200 shadow-sm sm:rounded-lg flex">
+                <div>
+                    <h2 class="font-bold text-4xl">
+                        {{ $tea->name }}
+                    </h2>
+                    <img class="border-b border-gray-400 shadow-lg sm:rounded-lg mt-5" src="{{ asset('storage/images/lyons_tea.jpeg') }}" width="250">
+                </div>
+
+                <div class="ml-6">
+                    <p class="mt-20 text-1xl text-gray-500"><em>Brand :</em> {{$tea->brand}}</p>
+                    <p class="mt-3 whitespace-">{{$tea->description}}</p>
+                    <p class="mt-3">Find us at: {{$tea->location}}</p>
+                    <p class="mt-3 font-serif text-2xl">€{{$tea->price }}</p>
+                </div>
+                
             </div>
+
+
         </div>
     </div>
 </x-app-layout>
