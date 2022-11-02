b5a370371763895efd03b2070ddc2da5d6cc3002
Agne Postnikova <N00211108@iadt.ie>
2022-11-02 20:10:12 +0000

    Finished create and update

--- app/Http/Controllers/TeaController.php
+++ app/Http/Controllers/TeaController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Tea;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-// use App\Http\Requests\StoreTeaRequest;   
+use App\Http\Requests\StoreTeaRequest;   
 use Illuminate\Support\Str;
 
 
@@ -18,6 +18,7 @@ class TeaController extends Controller
      */
     public function index()
     {
+        //This will show all the teas i have in the DB, it will show them by the latest update (recent first) and only show max 5 on page 1
         $teas = Tea::where('user_id', Auth::id())->latest('updated_at')->paginate(5);
         // dd($teas);
         return view('teas.index')->with('teas', $teas);
@@ -30,7 +31,8 @@ class TeaController extends Controller
      */
     public function create()
     {
-        //
+        //directs click to the create.blade.php page
+        return view('teas.create');
     }
 
     /**
@@ -41,8 +43,38 @@ class TeaController extends Controller
      */
     public function store(Request $request)
     {
-        // $tea_img = $request->file('tea_img');
-        // $path = $tea_img->storeAs('public/images', $filename);
+        //validating all the input fields and adding custom lengths
+        $request->validate([
+            'name' => 'required|max:50',
+            'brand' => 'required|max:50',
+            'description' => 'required|max:500',
+            'price' => 'required|max:15',
+            'tea_img' => 'file|image|dimensions:width=250,height=250',
+            'location' => 'required|max:120'
+        ]);
+        $tea_img = $request->file('tea_img');
+        $extention = $tea_img->getClientOriginalExtension();
+
+        //Changed the date formar to day-month-year
+        $filename = date('d-m-Y') . '_' . $request->input('name') . '.'. $extention;
+
+        //saying where to store the new images 
+        $path = $tea_img->storeAs('public/images', $filename);
+
+        //creating (well... adding) a new tea
+        Tea::create([
+            'uuid' => Str::uuid(),
+            'name' => $request->name,
+            'brand' => $request->brand,
+            'description' => $request->description,
+            'price' => $request->price,
+            'tea_img' => $filename,
+            'location' => $request->location,
+            'user_id' => Auth::id()
+        ]);
+
+        return to_route('teas.index')->with('success','Note created successfully');
+        // the "with" part makes a pop up notification to alert the user that they have successfully created a tea. 
     }
 
     /**
@@ -51,9 +83,9 @@ class TeaController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Tea $tea)
     {
-        $tea = Tea::where('id', $id)->where('user_id',Auth::id())->firstOrFail();
+        $tea = Tea::where('id', $tea->id)->where('user_id',Auth::id())->firstOrFail();
         return view('teas.show')->with('tea', $tea);
     }
 
@@ -63,9 +95,12 @@ class TeaController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Tea $tea)
     {
-        //
+        if($tea->user_id != Auth::id()) {
+            return abort(403);
+        }
+        return view('teas.edit')->with('tea', $tea);
     }
 
     /**
@@ -75,9 +110,35 @@ class TeaController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Tea $tea)
     {
-        //
+        if($tea->user_id != Auth::id()) {
+            return abort(403);
+        }
+        $request->validate([
+            'name' => 'required|max:50',
+            'brand' => 'required|max:50',
+            'description' => 'required|max:500',
+            'price' => 'required|max:15',
+            'tea_img' => 'file|image|dimensions:width=250,height=250',
+            'location' => 'required|max:120'
+        ]);    
+        $tea_img = $request->file('tea_img');
+        $extention = $tea_img->getClientOriginalExtension();
+
+        $filename = date('d-m-Y') . '_' . $request->input('name') . '.'. $extention;
+
+        $path = $tea_img->storeAs('public/images', $filename);
+        $tea->update([
+            'name' => $request->name,
+            'brand' => $request->brand,
+            'description' => $request->description,
+            'price' => $request-> price,
+            'tea_img' =>$filename,
+            'location' => $request-> location
+        ]);
+
+        return to_route('teas.show', $tea)->with('success','Tea updated successfully');
     }
 
     /**
